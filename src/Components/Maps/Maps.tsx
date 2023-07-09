11365d6e6966c7aec9dfc52cf4482e5ec116e1cf
yosukeeeey <yosuke-f-03298600@outlook.jp>
2023-07-09 23:33:13 +0900

    commit(delete api-key, site update)

--- src/Components/Maps/Maps.tsx
+++ src/Components/Maps/Maps.tsx
@@ -1,82 +1,32 @@
-/*import React, {useRef} from 'react';
-import { Loader } from "@googlemaps/js-api-loader"
-
-import key from './api-key.json'; 
-
-const Apikey = key['api-key'];
-
-export function Map() {
-     const loader = new Loader({
-        apiKey: `${Apikey}`,
-        version: "weekly",
-        // something additional options
-      });
-    
-    loader.load().then(() => {
-        const geocoder = new google.maps.Geocoder();
-        
-        geocoder.geocode({ address: "東京都千代田区" }, (results, status) => {
-          if (status == "OK" && results![0].geometry?.location !== undefined) {
-            console.log(results![0].geometry?.location);
-          }
-        });
-
-        let map: google.maps.Map; 
-        const center: google.maps.LatLngLiteral = {lat: 30, lng: -110};
-
-        function initMap(): React.FC {
-        map =  new google.maps.Map(document.getElementById("map") as HTMLElement, {
-            center,
-            zoom: 8
-        });
-
-        const para = document.createElement('map');
-        para.innerHTML = map;
-
-        return(map);
-        }
-
-        const ReactHTMLelement: React.FC = () => {
-            return(
-                initMap();
-            )
-        }        
-      });
-
-    return(
-        <React.Fragment>
-            <React.Component>
-                
-            </React.Component>    
-        </React.Fragment>
-    )
-}*/
+/**
+ * Mapを描画する(Leafret, React-Leafret)
+ */
 
 import React, { useState } from "react";
 import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import L from 'leaflet';
-
 import icon from 'leaflet/dist/images/marker-icon.png';
-import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
 
 let DefaultIcon = L.icon({
     iconUrl: icon,
-    shadowUrl: iconShadow
+    iconSize: [20,30],
+    iconAnchor: [12,36]
 });
 
 L.Marker.prototype.options.icon = DefaultIcon;
 
 const Map = () => {
   const [position, setPosition] = useState({
-    lat: 35.32428414114691,
+    lat: 35.32328414114691,
     lng: 138.86903111219146,
   });
   const [zoom, setZoom] = useState(15);
 
   return (
     <div>
-      <MapContainer center={position} zoom={zoom} style={{ height: "50vh" }}>
+      <MapContainer center={position} zoom={zoom} style={{ height: "30vh" }}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
--- src/Jeberry.tsx
+++ src/Jeberry.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import Typography from '@mui/material/Typography'
 import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
 
 import AppBar from './Components/Header/AppBar';
 import BackImage from './Components/background_green.jpg';
 import Map from './Components/Maps/Maps';
+import { Tab } from "@mui/base";
+
+const iconStyle = {
+    width: 48,
+    height: 48,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'warning.main',
+    mr: 1
+};
 
 function Jeberry() {
     return(
@@ -41,19 +53,97 @@ function Jeberry() {
             </div>
             
             {/* イメージ御殿場、場所 */}
-            <div style={{backgroundImage:`url(${BackImage})`, backgroundSize:'100%'}}>
+            <div style={{backgroundColor:'darkseagreen', backgroundSize:'100%'}}>
+
+                <div style={{width: '80%', height: '80%', margin:'0px auto', padding:'10%'}}>
+                <Typography variant="h5" textAlign='center'>Location</Typography>
+                <Typography variant='h6' textAlign='center'>国立中央青少年交流の家</Typography>
+                <Typography variant='body1' textAlign='center'>〒412-0006　静岡県御殿場市中畑2092-5</Typography>
+                <Typography variant="body1" sx={{margin: 2}}></Typography>
                 <Map />
+                <Typography variant='body1' textAlign='center'>
+                    <br></br>詳細は
+                    <a href="https://fujinosato.niye.go.jp/">こちら</a>
+                    をクリック
+                </Typography>
+                
+                </div>
             </div>
-            
 
             {/* 日程(プログラム) */}
+            <div style={{backgroundColor:'cornflowerblue', backgroundSize:'100%'}}>
+            <Grid container  direction="column" justifyContent="center"  >
+                <Typography variant="h5" textAlign='center'>Program</Typography>
+                <Typography variant="body1" sx={{margin: 2}}></Typography>
 
+                    <Typography variant="h6" textAlign='center'>Day 1</Typography>
+                    <Typography variant="h6" textAlign='center'>2023/09/15 Sat </Typography>
+                    <Typography variant="body1" sx={{margin: 1}}></Typography>
+                        <div style={{width: '80%', height: '80%', margin:'0px auto', padding:'5%', border:'solid'}}>
+                            <Typography variant="body1">09:00 新宿駅集合</Typography>
+                            <Typography variant="body1">13:00 OPENING</Typography>
+                        </div>
+                    <Typography variant="body1" sx={{margin: 2}}></Typography>
+
+                    <Typography variant="h6" textAlign='center'>Day 2</Typography>
+                    <Typography variant="h6" textAlign='center'>2023/09/16 Sun </Typography>
+                    <Typography variant="body1" sx={{margin: 1}}></Typography>
+                        <div style={{width: '80%', height: '80%', margin:'0px auto', padding:'5%', border:'solid'}}>
+                            <Typography variant="body1">08:00 朝食@食堂</Typography>
+                            <Typography variant="body1">09:00 QT(各SG)</Typography>
+                            <Typography variant="body1">10:30 特別礼拝@全体ホール</Typography>
+                            <Typography variant="body1">12:00 ランチ＠食堂</Typography>
+                        </div>
+                    <Typography variant="body1" sx={{margin: 2}}></Typography>
+
+                    <Typography variant="h6" textAlign='center'>Day 3</Typography>
+                    <Typography variant="h6" textAlign='center'>2023/09/17 Mon </Typography>
+                    <Typography variant="body1" sx={{margin: 1}}></Typography>
+                        <div style={{width: '80%', height: '80%', margin:'0px auto', padding:'5%', border:'solid'}}>
+                            <Typography variant="body1">08:00 朝食@食堂</Typography>
+                            <Typography variant="body1">09:00 QT(各SG)</Typography>
+                            <Typography variant="body1">10:30 特別クラス</Typography>
+                            <Typography variant="body1">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;①聖書クラス</Typography>
+                            <Typography variant="body1">      ②NDCクラス</Typography>
+                            <Typography variant="body1">12:00 ランチ＠食堂</Typography>
+                        </div>
+                    <Typography variant="body1" sx={{margin: 2}}></Typography>
+
+            </Grid>
+            </div>
             {/* お知らせ */}
+            <div style={{backgroundColor:'blueviolet', backgroundSize:'100%'}}>
+            <Grid container  direction="column" justifyContent="center">
+                <Typography variant="h5" textAlign='center'>Announcement</Typography>
+                <Typography variant="body1" sx={{margin: 2}}></Typography>
+            </Grid>
+            </div>
 
             {/* 讃美歌集  Linkで飛ばす */}
+            <div style={{backgroundColor:'cadetblue', backgroundSize:'100%'}}>
+            <Grid container  direction="column" justifyContent="center">
+                <Typography variant="h5" textAlign='center'>Worship lrycs</Typography>
+                <Typography variant="body1" sx={{margin: 2}}></Typography>
+            </Grid>
+            </div>
 
             {/* その他 Footer Webページ、インスタ、Twitter */}
+            <div style={{backgroundColor:'lightgray', backgroundSize:'100%'}}>
+            <Grid container  direction="column" justifyContent="center">
+                <Typography variant="h5" textAlign='center'>Others</Typography>
+                <Typography variant="body1" sx={{margin: 2}}></Typography>
+            
+                <Box component="a" href="https://www.instagram.com/werlife2.0/" sx={iconStyle}>
+                    <img
+                        src={`${process.env.PUBLIC_URL}/instagram_icon.png`}
+                        alt="Instagram"
+                        width={48}
+                        height={48}
+                    />
+                </Box>
+            </Grid>
 
+            </div>
 
         </React.Fragment>
     )
